ba938ff850bd9ec1a3d19f1c4e8323dd5c61e3a3
albertnanda <albert.nanda@gmail.com>
2016-10-19 16:37:45 +0530

    Upsampling test RMSe=1793.321

--- XGB_Baseline.R
+++ XGB_Baseline.R
@@ -1,4 +1,7 @@
-#Kaggle Competition #Fresh Attempt
+#Kaggle Competition
+#clean memory
+rm(list = ls())
+gc()
 #Load the libraries
 require(caret)
 require(corrplot)
@@ -10,15 +13,17 @@ require(h2o)
 require(readr)
 require(stringr)
 require(Matrix)
-library(xgboost)
+require(xgboost)
+require(data.table)
 
 #read the data set
 setwd("d:/Kaggle Competition/")
-raw_data=read.csv("train.csv",header=T)
-
+raw_data=fread("train.csv",stringsAsFactors = T)
+raw_data=data.table(raw_data)
 
 #splitting train and test data for grouped categories
 set.seed(1)
+index=createDataPartition(raw_data$cat116,p=.8,list = F)
 index = sort(sample(nrow(raw_data), nrow(raw_data)*.8))
 train=raw_data[index,]
 test=raw_data[-index,]
@@ -35,8 +40,8 @@ dtest <- xgb.DMatrix(data =test_matrix, label = test$loss)
 watchlist <- list(train=dtrain, test=dtest)
 set.seed(1)
 xgb=xgboost(data = dtrain,
-            nrounds = 2000,
-            eta=.01,
+            nrounds = 200,
+            eta=.1,
             max_depth= 15,
             subsample=.5,
             booster="gbtree",
@@ -51,30 +56,31 @@ xgb_pred=predict(xgb,test_matrix)
 
 #Data Processing
 
+##Cut the sample into two parts
+quantile(raw_data$loss,prob=seq(.9,1,length=11),type = 5)
 
-
-
-
-
-
-
+raw_data_extreme=raw_data[raw_data$loss>=15000,]
+dim(raw_data)
+nrow(raw_data_extreme)
+raw_data_normal=raw_data[raw_data$loss<6400,]
+quantile(raw_data_extreme$loss,prob=seq(0,1,length=11),type=5)
 
 #parameter Tuning
 
 xgb.grid <- expand.grid(nrounds = 100,
-                        eta = c(0.01,0.1),
-                        max_depth = 15,
+                        eta = .01,
+                        max_depth = 10,
                         gamma=1,
                         colsample_bytree=.5,
                         min_child_weight=1)
 set.seed(1)
 trCtrl=trainControl(method = "cv")
-xgbTune=train(loss~.-id,data=train,method="xgbTree",
+xgbTune=train(loss~.-id,data=raw_data_extreme,method="xgbTree",
               trControl=trCtrl,
               subsample=.5,
               tuneGrid=xgb.grid,
               booster="gbtree",
               objective = "reg:linear",
-              eval_metric="rmse",
-              watchlist=watchlist
-)
+              eval_metric="rmse"
+    )
+xgbTune
