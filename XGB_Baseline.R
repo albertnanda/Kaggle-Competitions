b84d828535d9aded43cac6d53d33518fde39d4a1
albertnanda <albert.nanda@gmail.com>
2016-10-22 12:23:13 +0530

    loss+mean(loss)

--- XGB_Baseline.R
+++ XGB_Baseline.R
@@ -17,10 +17,21 @@ require(xgboost)
 require(data.table)
 require(Cubist)
 
+
+
 #read the data set
 setwd("d:/Kaggle Competition/")
 raw_data=fread("train.csv",stringsAsFactors = T)
 raw_data=data.table(raw_data)
+
+#save the original variable
+save_loss=raw_data$loss
+
+
+#log transform the variable
+raw_data$loss=log(raw_data$loss+mean(raw_data$loss))
+
+
 
 #create sparse matrix
 data_matrix=sparse.model.matrix(id+loss~.-1,data=raw_data)
@@ -31,28 +42,41 @@ ddata <- xgb.DMatrix(data = data_matrix, label = raw_data$loss)
 set.seed(1)
 param=list("objective" = "reg:linear",
            "eval_metric" = "rmse",
-           "eta"=.11,
-           "max_depth"= 15,
-           "subsample"=.5,
-           "colsample_bytree"=.5,
-           "min_child_weight"=1
+           "eta"=.2,
+           "max_depth"= 8,
+           "subsample"=.4,
+           "colsample_bytree"=.3,
+           "min_child_weight"=2
            )
 
-xgb=xgb.cv(params = param,data = ddata,nrounds = 100,nfold = 10)
-  
-  
-xgb_pred=predict(xgb,test_matrix)
-(RMSE_xgb=sqrt(mean((xgb_pred-raw_data$loss)^2)))
+xgb=xgb.cv(params = param,data = ddata,nrounds = 30,nfold = 10)
+
+
+
+xgb_final=xgb.train(params = param,data = ddata,nrounds = 30)
+
+
+
+test=fread("test.csv",stringsAsFactors = T)
+test=data.table(test)
+#create sparse matrix
+test_matrix=sparse.model.matrix(id~.-1,data=test)
+
+test_xgb <- xgb.DMatrix(data = test_matrix)
 
 
 
 
+xgb_pred=predict(xgb_final,test_xgb)
+head(xgb_pred)
+exp(max(xgb_pred))
+xgb_pred=exp(xgb_pred)
 
+test$loss=xgb_pred
 
-
-
-
-
+test=test[,c(1,ncol(test)),with=F]
+write.csv(test,"logtest2.csv")
+max(test$loss)
 
 
 
